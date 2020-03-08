ebd549da1c8e229b12fe6893dade00c0b3d9708e
Jonathan Pedigo <jon.pedigo@feedly.com>
2020-03-08 14:40:06 -0700

    add client to pathgit add -A god..

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/dist/index.html')
+  res.sendFile(__dirname + '/client/dist/index.html')
 })
 
 // Set static file location for production
-app.use(express.static(require('path').resolve('./dist')))
+app.use(express.static(require('path').resolve('./client/dist')))
 
 let serverState = []
 let grid = []
