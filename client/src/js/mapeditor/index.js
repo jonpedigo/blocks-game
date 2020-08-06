41ffbe81c95f8fb3724c5af4cdfc24584a3b34ce
Nathan Anecone <nanecone@gmail.com>
2020-08-06 14:22:24 -0400

    add user menu folder

--- client/src/js/mapeditor/index.js
+++ client/src/js/mapeditor/index.js
@@ -1,12 +1,13 @@
 import gridUtil from '../utils/grid.js'
 import collisionsUtil from '../utils/collisions'
 import adminContextMenu from './adminMenus/contextMenu.jsx'
+import userContextMenu from './userMenus/userContextMenu.jsx'
 import selectionTools from './selectionTools';
 import keyInput from './keyInput';
 import render from './render';
 import Camera from '../map/camera'
 
-class MapEditor{
+class MapEditor {
   constructor() {
     this.initState()
   }
@@ -44,20 +45,23 @@ class MapEditor{
     MAPEDITOR.camera = camera
 
     document.body.addEventListener("mousedown", (e) => {
-      if(!MAPEDITOR.paused) handleMouseDown(event)
+      if (!MAPEDITOR.paused) handleMouseDown(event)
     })
     document.body.addEventListener("mousemove", (e) => {
-      if(!MAPEDITOR.paused) handleMouseMove(event)
+      if (!MAPEDITOR.paused) handleMouseMove(event)
     })
     document.body.addEventListener("mouseup", (e) => {
-       if(!MAPEDITOR.paused) handleMouseUp(event)
+      if (!MAPEDITOR.paused) handleMouseUp(event)
     })
     document.body.addEventListener("mouseout", (e) => {
-       if(!MAPEDITOR.paused) handleMouseOut(event)
+      if (!MAPEDITOR.paused) handleMouseOut(event)
     })
 
-    if(PAGE.role.isAdmin) {
+    if (PAGE.role.isAdmin) {
       adminContextMenu.init(MAPEDITOR)
+    }
+    if (PAGE.role.isPlayer) {
+      userContextMenu.init(MAPEDITOR)
     }
     keyInput.init()
 
@@ -71,9 +75,9 @@ class MapEditor{
     MAPEDITOR.initState()
     MAPEDITOR.pause()
 
-    const removeListener = window.local.on('onConstructEditorClose', ({constructParts, x, y, width, height}) => {
-      if(constructParts) {
-        window.socket.emit('editObjects', [{ id: object.id, constructParts, spawnPointX: x, spawnPointY: y, x, y, width, height}])
+    const removeListener = window.local.on('onConstructEditorClose', ({ constructParts, x, y, width, height }) => {
+      if (constructParts) {
+        window.socket.emit('editObjects', [{ id: object.id, constructParts, spawnPointX: x, spawnPointY: y, x, y, width, height }])
       }
       window.socket.emit('editGameState', { paused: false })
       MAPEDITOR.resume()
@@ -90,12 +94,12 @@ class MapEditor{
   }
 
   onUpdate(delta) {
-    if(MAPEDITOR.remoteState && !MAPEDITOR.skipRemoteStateUpdate) {
+    if (MAPEDITOR.remoteState && !MAPEDITOR.skipRemoteStateUpdate) {
       updateGridHighlight(MAPEDITOR.remoteState.mousePos)
     }
 
-    if(!PAGE.role.isGhost && PAGE.role.isPlayer && GAME.heros[HERO.id]) {
-      window.socket.emit('sendHeroMapEditor', { mousePos: MAPEDITOR.mousePos } , HERO.id)
+    if (!PAGE.role.isGhost && PAGE.role.isPlayer && GAME.heros[HERO.id]) {
+      window.socket.emit('sendHeroMapEditor', { mousePos: MAPEDITOR.mousePos }, HERO.id)
     }
   }
 
@@ -131,41 +135,41 @@ class MapEditor{
   }
 
   onSendHeroMapEditor(remoteState, heroId) {
-    if(GAME.heros[HERO.id] && HERO.id === heroId) {
+    if (GAME.heros[HERO.id] && HERO.id === heroId) {
       MAPEDITOR.remoteState = remoteState
     }
   }
 
   onAskHeroToNameObject(object, heroId) {
-    if(PAGE.role.isPlayer && !PAGE.role.isGhost && HERO.id === heroId) {
+    if (PAGE.role.isPlayer && !PAGE.role.isGhost && HERO.id === heroId) {
       modals.nameObject(object)
     }
   }
 
   onAskHeroToWriteDialogue(object, heroId) {
-    if(PAGE.role.isPlayer && !PAGE.role.isGhost && HERO.id === heroId) {
+    if (PAGE.role.isPlayer && !PAGE.role.isGhost && HERO.id === heroId) {
       modals.writeDialogue(object)
     }
   }
 
   networkEditObject(object, update) {
-    if(object.tags.subObject && object.subObjectName && object.ownerId) {
+    if (object.tags.subObject && object.subObjectName && object.ownerId) {
       const owner = OBJECTS.getOwner(object)
       window.socket.emit('editSubObject', object.ownerId, object.subObjectName, update)
-    } else if(object.tags.hero) {
-      window.socket.emit('editHero', {id: object.id, ...update})
+    } else if (object.tags.hero) {
+      window.socket.emit('editHero', { id: object.id, ...update })
     } else {
-      window.socket.emit('editObjects', [{id: object.id, ...update}])
+      window.socket.emit('editObjects', [{ id: object.id, ...update }])
     }
   }
 
   deleteObject(object) {
-    if(object.tags.subObject && object.subObjectName && object.ownerId) {
+    if (object.tags.subObject && object.subObjectName && object.ownerId) {
       const owner = OBJECTS.getOwner(object)
       window.socket.emit('deleteSubObject', owner, object.subObjectName)
-    } else if(object.tags.hero) {
+    } else if (object.tags.hero) {
       window.socket.emit('deleteHero', object)
-    } else if(object.id) {
+    } else if (object.id) {
       window.socket.emit('deleteObject', object)
     } else {
       console.error('trying to delete object without id')
@@ -175,9 +179,9 @@ class MapEditor{
   }
 
   removeObject(object) {
-    if(object.tags.subObject && object.subObjectName && object.ownerId) {
+    if (object.tags.subObject && object.subObjectName && object.ownerId) {
       window.socket.emit('removeSubObject', object.ownerId, object.subObjectName)
-    } else if(object.tags.hero) {
+    } else if (object.tags.hero) {
       window.socket.emit('removeHero', object)
     } else {
       window.socket.emit('removeObject', object)
@@ -197,14 +201,14 @@ function handleMouseDown(event) {
   MAPEDITOR.clickStart.x = ((event.clientX + camera.x) / camera.multiplier)
   MAPEDITOR.clickStart.y = ((event.clientY + camera.y) / camera.multiplier)
 
-  if(MAPEDITOR.copiedObject) {
+  if (MAPEDITOR.copiedObject) {
     OBJECTS.create([MAPEDITOR.copiedObject])
     MAPEDITOR.copiedObject = null
-  } else if(MAPEDITOR.isSettingPathfindingLimit) {
-    if(MAPEDITOR.pathfindingLimit) {
+  } else if (MAPEDITOR.isSettingPathfindingLimit) {
+    if (MAPEDITOR.pathfindingLimit) {
       const { pathfindingLimit, objectHighlighted } = MAPEDITOR
-      gridUtil.snapDragToGrid(pathfindingLimit, {dragging: true})
-      networkEditObject(objectHighlighted, {id: objectHighlighted.id, pathfindingLimit, path: null})
+      gridUtil.snapDragToGrid(pathfindingLimit, { dragging: true })
+      networkEditObject(objectHighlighted, { id: objectHighlighted.id, pathfindingLimit, path: null })
       document.body.style.cursor = "default";
       MAPEDITOR.isSettingPathfindingLimit = false
       MAPEDITOR.pathfindingLimit = null
@@ -213,25 +217,25 @@ function handleMouseDown(event) {
       MAPEDITOR.pathfindingLimit.width = 0
       MAPEDITOR.pathfindingLimit.height = 0
     }
-  } else if(MAPEDITOR.resizingObject) {
+  } else if (MAPEDITOR.resizingObject) {
     const { resizingObject } = MAPEDITOR
-    networkEditObject(resizingObject, {id: resizingObject.id, x: resizingObject.x, y: resizingObject.y, width: resizingObject.width, height: resizingObject.height})
+    networkEditObject(resizingObject, { id: resizingObject.id, x: resizingObject.x, y: resizingObject.y, width: resizingObject.width, height: resizingObject.height })
     MAPEDITOR.resizingObject = null
-  } else if(MAPEDITOR.draggingObject) {
+  } else if (MAPEDITOR.draggingObject) {
     const { draggingObject } = MAPEDITOR
-    if(draggingObject.constructParts) {
-      networkEditObject(draggingObject, {id: draggingObject.id, spawnPointX: draggingObject.x, y: draggingObject.y, spawnPointY: draggingObject.y, x: draggingObject.x, y: draggingObject.y, constructParts: draggingObject.constructParts})
-    } else if(GAME.gameState.started) {
-      networkEditObject(draggingObject, {id: draggingObject.id, x: draggingObject.x, y: draggingObject.y})
+    if (draggingObject.constructParts) {
+      networkEditObject(draggingObject, { id: draggingObject.id, spawnPointX: draggingObject.x, y: draggingObject.y, spawnPointY: draggingObject.y, x: draggingObject.x, y: draggingObject.y, constructParts: draggingObject.constructParts })
+    } else if (GAME.gameState.started) {
+      networkEditObject(draggingObject, { id: draggingObject.id, x: draggingObject.x, y: draggingObject.y })
     } else {
-      networkEditObject(draggingObject, {id: draggingObject.id, x: draggingObject.x, spawnPointX: draggingObject.x, y: draggingObject.y, spawnPointY: draggingObject.y})
+      networkEditObject(draggingObject, { id: draggingObject.id, x: draggingObject.x, spawnPointX: draggingObject.x, y: draggingObject.y, spawnPointY: draggingObject.y })
     }
     MAPEDITOR.draggingObject = null
-  } else if(MAPEDITOR.draggingRelativeObject) {
+  } else if (MAPEDITOR.draggingRelativeObject) {
     const { draggingRelativeObject } = MAPEDITOR
     const owner = OBJECTS.getOwner(draggingRelativeObject)
     const { relativeX, relativeY } = OBJECTS.getRelativeCenterXY(draggingRelativeObject, owner)
-    networkEditObject(draggingRelativeObject, {id: draggingRelativeObject.id, relativeX, relativeY })
+    networkEditObject(draggingRelativeObject, { id: draggingRelativeObject.id, relativeX, relativeY })
     MAPEDITOR.draggingRelativeObject = null
   }
 
@@ -239,7 +243,7 @@ function handleMouseDown(event) {
 }
 
 function handleMouseOut(event) {
-  if(PAGE.role.isGhost) {
+  if (PAGE.role.isGhost) {
     MAPEDITOR.skipRemoteStateUpdate = false
   }
 }
@@ -247,36 +251,36 @@ function handleMouseOut(event) {
 function handleMouseMove(event) {
   const { camera } = MAPEDITOR
 
-  if(!window.isClickingMap(event.target.className)) return
+  if (!window.isClickingMap(event.target.className)) return
 
-  if(PAGE.role.isGhost) {
+  if (PAGE.role.isGhost) {
     MAPEDITOR.skipRemoteStateUpdate = true
   }
 
   MAPEDITOR.mousePos.x = ((event.clientX + camera.x) / camera.multiplier)
   MAPEDITOR.mousePos.y = ((event.clientY + camera.y) / camera.multiplier)
 
-  if(MAPEDITOR.draggingRelativeObject) {
+  if (MAPEDITOR.draggingRelativeObject) {
     updateDraggingObject(MAPEDITOR.draggingRelativeObject)
-  } else if(MAPEDITOR.copiedObject) {
+  } else if (MAPEDITOR.copiedObject) {
     updateDraggingObject(MAPEDITOR.copiedObject)
-  } else if(MAPEDITOR.isSettingPathfindingLimit) {
-    if(MAPEDITOR.pathfindingLimit) {
+  } else if (MAPEDITOR.isSettingPathfindingLimit) {
+    if (MAPEDITOR.pathfindingLimit) {
       updateResizingObject(MAPEDITOR.pathfindingLimit, { allowTiny: false })
     }
-  } else if(MAPEDITOR.resizingObject) {
+  } else if (MAPEDITOR.resizingObject) {
     updateResizingObject(MAPEDITOR.resizingObject)
-  } else if(MAPEDITOR.draggingObject) {
+  } else if (MAPEDITOR.draggingObject) {
     updateDraggingObject(MAPEDITOR.draggingObject)
   } else {
-    updateGridHighlight({x: MAPEDITOR.mousePos.x, y: MAPEDITOR.mousePos.y})
+    updateGridHighlight({ x: MAPEDITOR.mousePos.x, y: MAPEDITOR.mousePos.y })
   }
 }
 
 function updateGridHighlight(location) {
-  if(MAPEDITOR.contextMenuVisible) return
+  if (MAPEDITOR.contextMenuVisible) return
 
-  const { x,y } = gridUtil.snapXYToGrid(location.x, location.y, { closest: false })
+  const { x, y } = gridUtil.snapXYToGrid(location.x, location.y, { closest: false })
 
   let mouseLocation = {
     x,
@@ -288,43 +292,43 @@ function updateGridHighlight(location) {
   MAPEDITOR.objectHighlighted = mouseLocation
 
   // find the smallest one stacked up
-  let smallestObject = selectionTools.findSmallestObjectInArea(mouseLocation, GAME.objects.filter(({reserved}) => !reserved))
+  let smallestObject = selectionTools.findSmallestObjectInArea(mouseLocation, GAME.objects.filter(({ reserved }) => !reserved))
 
   collisionsUtil.check(mouseLocation, GAME.heroList, (hero) => {
-    if(hero.removed) return
+    if (hero.removed) return
     smallestObject = JSON.parse(JSON.stringify(hero))
   })
 
-  if(smallestObject) MAPEDITOR.objectHighlighted = JSON.parse(JSON.stringify(smallestObject))
+  if (smallestObject) MAPEDITOR.objectHighlighted = JSON.parse(JSON.stringify(smallestObject))
 
   MAPEDITOR.objectHighlightedChildren = []
-  if(MAPEDITOR.objectHighlighted.id || MAPEDITOR.objectHighlighted.ownerId) {
+  if (MAPEDITOR.objectHighlighted.id || MAPEDITOR.objectHighlighted.ownerId) {
     // see if grid high light has children or is child
     const { parent, children } = selectionTools.getObjectRelations(MAPEDITOR.objectHighlighted, GAME)
-    if(parent.constructParts) {
+    if (parent.constructParts) {
       MAPEDITOR.objectHighlighted = parent
-    } else if(children.length && parent.id === MAPEDITOR.objectHighlighted.id) {
+    } else if (children.length && parent.id === MAPEDITOR.objectHighlighted.id) {
       MAPEDITOR.objectHighlighted = parent
       MAPEDITOR.objectHighlightedChildren = children
     }
   }
 }
 
-function updateResizingObject(object, options = { allowTiny : true }) {
+function updateResizingObject(object, options = { allowTiny: true }) {
   const { mousePos } = MAPEDITOR
-  if(mousePos.x < object.x || mousePos.y < object.y) {
+  if (mousePos.x < object.x || mousePos.y < object.y) {
     return
   }
   object.width = mousePos.x - object.x
   object.height = mousePos.y - object.y
 
   let tinySize
-  if(object.width < GAME.grid.nodeSize - 4 && object.height < GAME.grid.nodeSize - 4 && options.allowTiny) {
+  if (object.width < GAME.grid.nodeSize - 4 && object.height < GAME.grid.nodeSize - 4 && options.allowTiny) {
     tinySize = object.width
   }
 
-  if(MAPEDITOR.snapToGrid) {
-    if(tinySize) {
+  if (MAPEDITOR.snapToGrid) {
+    if (tinySize) {
       gridUtil.snapTinyObjectToGrid(object, tinySize)
     } else {
       gridUtil.snapDragToGrid(object)
@@ -342,12 +346,12 @@ function updateDraggingObject(object) {
   object.y = mousePos.y
 
   let tinySize
-  if(object.width < GAME.grid.nodeSize - 4 && object.height < GAME.grid.nodeSize - 4) {
+  if (object.width < GAME.grid.nodeSize - 4 && object.height < GAME.grid.nodeSize - 4) {
     tinySize = object.width
   }
 
-  if(MAPEDITOR.snapToGrid) {
-    if(tinySize) {
+  if (MAPEDITOR.snapToGrid) {
+    if (tinySize) {
       gridUtil.snapTinyObjectToGrid(object, tinySize)
     } else {
       gridUtil.snapDragToGrid(object)
@@ -357,10 +361,10 @@ function updateDraggingObject(object) {
   const diffX = object.x - startX
   const diffY = object.y - startY
 
-  if(object.constructParts) {
+  if (object.constructParts) {
     object.constructParts.forEach((part) => {
-      part.x+= diffX
-      part.y+= diffY
+      part.x += diffX
+      part.y += diffY
       return part
     })
   }
---
+++ client/src/js/mapeditor/userMenus/userContextMenu.jsx
@@ -0,0 +1,233 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Menu, { SubMenu, MenuItem } from 'rc-menu';
+import { SwatchesPicker } from 'react-color';
+import HeroContextMenu from './heroContextMenu.jsx';
+import ObjectContextMenu from './objectContextMenu.jsx';
+import modals from '../modals.js'
+
+function init(editor, props) {
+    MAPEDITOR.contextMenu = document.getElementById('context-menu')
+    MAPEDITOR.contextMenuVisible = false
+
+    // Mount React App
+    ReactDOM.render(
+        React.createElement(contextMenuEl, { editor, ...props, ref: ref => MAPEDITOR.contextMenuRef = ref }),
+        MAPEDITOR.contextMenu
+    )
+}
+
+class contextMenuEl extends React.Component {
+    constructor(props) {
+        super(props)
+
+        document.body.addEventListener("contextmenu", e => {
+            if (!window.isClickingMap(e.target.className)) return
+            if (!MAPEDITOR.paused) {
+                e.preventDefault();
+                const origin = {
+                    left: e.pageX,
+                    top: e.pageY
+                };
+                this._setContextMenuPosition(origin);
+                return false;
+            }
+        });
+
+        window.addEventListener("click", e => {
+            if (e.target.className.indexOf('dont-close-menu') >= 0) {
+            } else {
+                this._toggleContextMenu("hide");
+            }
+        });
+
+        this.state = {
+            hide: true,
+            objectSelected: {},
+            subObjectSelected: {},
+            subObjectSelectedName: null,
+            coloringObject: null,
+        }
+
+        this.openColorPicker = (coloringObject) => {
+            this.setState({ coloringObject })
+        }
+
+        this._handleMapMenuClick = ({ key }) => {
+            const { objectSelected } = this.state
+
+            if (key === 'create-object') {
+                OBJECTS.create({ ...objectSelected, tags: { obstacle: true } })
+            }
+
+            if (key === 'toggle-pause-game') {
+                window.socket.emit('editGameState', { paused: !GAME.gameState.paused })
+            }
+
+            if (key === 'toggle-start-game') {
+                if (GAME.gameState.started) {
+                    window.socket.emit('stopGame')
+                } else {
+                    window.socket.emit('startGame')
+                }
+            }
+
+            if (key === 'set-world-respawn-point') {
+                window.socket.emit('updateWorld', { worldSpawnPointX: objectSelected.x, worldSpawnPointY: objectSelected.y })
+            }
+
+            if (key === 'select-world-background-color') {
+                this.openColorPicker('worldBackground')
+            }
+            if (key === 'select-default-object-color') {
+                this.openColorPicker('defaultObject')
+            }
+
+            if (key === 'open-sequence-editor') {
+                WORLDMANAGER.open('sequence')
+            }
+
+            if (key === 'download-game-JSON') {
+                let saveGame = GAME.cleanForSave(GAME)
+                console.log(saveGame)
+                PAGE.downloadObjectAsJson(saveGame, GAME.id)
+            }
+        }
+
+        this._handleGameTagMenuClick = ({ key }) => {
+            const { objectSelected } = this.state
+
+            if (key === 'create-game-tag') {
+                modals.addGameTag()
+                return
+            }
+
+            window.socket.emit('editObjects', [{ id: objectSelected.id, tags: { [key]: !objectSelected.tags[key] } }])
+        }
+
+        this._selectSubObject = (subObject, name) => {
+            MAPEDITOR.objectHighlighted = subObject
+            this.setState({
+                subObjectSelected: subObject,
+                subObjectSelectedName: name,
+            })
+        }
+    }
+
+    _toggleContextMenu(command) {
+        if (command === "show") {
+            this.setState({ hide: false, objectSelected: MAPEDITOR.objectHighlighted })
+        } else {
+            this.setState({ hide: true, subObjectSelected: {}, subObjectSelectedName: null })
+        }
+    }
+
+    _setContextMenuPosition({ top, left }) {
+        // THIS ADJUSTS THE SIZE OF THE CONTEXT MENU IF ITS TOO CLOSE TO THE EDGES
+        if (MAPEDITOR.objectHighlighted.id) {
+            const heightDesired = 350
+            const widthDesired = 450
+
+            const bottomDistance = window.innerHeight - top
+            const rightDistance = window.innerWidth - left
+
+            if (bottomDistance < heightDesired) {
+                top = window.innerHeight - heightDesired
+            }
+
+            if (rightDistance < widthDesired) {
+                left = window.innerWidth - widthDesired
+            }
+        }
+
+        MAPEDITOR.contextMenu.style.left = `${left}px`
+        MAPEDITOR.contextMenu.style.top = `${top}px`
+
+        this._toggleContextMenu('show')
+    }
+
+
+    _renderAdvancedWorldMenu() {
+        const { objectSelected } = this.props
+
+        return <SubMenu title="Advanced">
+            <MenuItem key='download-game-JSON'>Download Game JSON</MenuItem>
+            <MenuItem key='open-sequence-editor'>Open Sequence Editor</MenuItem>
+        </SubMenu>
+    }
+
+    render() {
+        const { hide, coloringObject, objectSelected, subObjectSelected, subObjectSelectedName } = this.state;
+        const { networkEditObject } = MAPEDITOR
+
+        if (hide) {
+            MAPEDITOR.contextMenuVisible = false
+            return null
+        }
+
+        if (coloringObject) {
+            return <SwatchesPicker
+                color={coloringObject.color}
+                onChange={(color) => {
+                    if (coloringObject == 'worldBackground') {
+                        window.socket.emit('updateWorld', { backgroundColor: color.hex })
+                    } else if (coloringObject == 'defaultObject') {
+                        window.socket.emit('updateWorld', { defaultObjectColor: color.hex })
+                    } else {
+                        coloringObject.tags.outline = false
+                        networkEditObject(coloringObject, { color: color.hex })
+                    }
+                    this.setState({
+                        coloringObject: null,
+                    })
+                }}
+            />
+        }
+
+        // if(GAME.gameState.started && !PAGE.role.isAdmin) {
+        //   return <Menu onClick={this._handleMapMenuClick}>
+        //     <MenuItem key='toggle-pause-game'>{ GAME.gameState.paused ? 'Unpause game' : 'Pause game' }</MenuItem>
+        //     <MenuItem key='toggle-start-game'>{ GAME.gameState.started ? 'Stop Game' : 'Start Game' }</MenuItem>
+        //   </Menu>
+        // }
+
+        MAPEDITOR.contextMenuVisible = true
+
+        if (subObjectSelected && subObjectSelectedName) {
+            return <ObjectContextMenu
+                objectSelected={subObjectSelected}
+                openColorPicker={this.openColorPicker}
+                selectSubObject={this._selectSubObject}
+                subObject
+            />
+        }
+
+        if (objectSelected.tags && objectSelected.tags.hero) {
+            return <HeroContextMenu
+                objectSelected={objectSelected}
+                openColorPicker={this.openColorPicker}
+                selectSubObject={this._selectSubObject}
+            />
+        }
+
+        if (!objectSelected.id) {
+            return <Menu onClick={this._handleMapMenuClick}>
+                <MenuItem key='create-object'>Create object</MenuItem>
+                <MenuItem key='set-world-respawn-point'>Set as world respawn point</MenuItem>
+                <MenuItem className='dont-close-menu' key='select-world-background-color'>Set world background color</MenuItem>
+                <MenuItem className='dont-close-menu' key='select-default-object-color'>Set default object color</MenuItem>
+                <MenuItem key='toggle-start-game'>{GAME.gameState.started ? 'Stop Game' : 'Start Game'}</MenuItem>
+            </Menu>
+        }
+
+        return <ObjectContextMenu
+            objectSelected={objectSelected}
+            openColorPicker={this.openColorPicker}
+            selectSubObject={this._selectSubObject}
+        />
+    }
+}
+
+export default {
+    init
+}
