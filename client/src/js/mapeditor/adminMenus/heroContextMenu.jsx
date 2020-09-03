1935ecd7c6db8944b94c0e1bbeca1f8d666ec4c6
Jonathan Pedigo <jon.pedigo@feedly.com>
2020-09-03 14:15:49 -0500

    fix

--- client/src/js/mapeditor/adminMenus/heroContextMenu.jsx
+++ client/src/js/mapeditor/adminMenus/heroContextMenu.jsx
@@ -186,7 +186,7 @@ export default class HeroContextMenu extends React.Component {
         {this._renderDeleteQuestList(objectSelected.quests)}
       </SubMenu>
       <SubMenu title="Tags">
-        <CurrentsTagMenu objectSelected={objectSelected} currentTags={objectSelected.tags}></CurrentTagMenu>
+        <CurrentsTagMenu objectSelected={objectSelected} currentTags={objectSelected.tags}></CurrentTagsMenu>
       </SubMenu>
       <SubMenu title="All Tags">
         <TagMenu objectSelected={objectSelected}></TagMenu>
